e4413b35c04674dc634fa6bd2621b7813a9ba410
pikrinwerdiansyh76 <sanggili.official.com>
2024-05-25 04:28:49 +0700

    first commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  KEY: "fbe71214984051d92138d8ac3fc254fd",
+  BASE_URL: "https://api.themoviedb.org/3/",
+  BASE_IMAGE_URL: "https://image.tmdb.org/t/p/w500/",
+  DEFAULT_LANGUAGE: "en-us",
+  CACHE_NAME: new Date().toISOString(),
+};
+
+export default CONFIG;
